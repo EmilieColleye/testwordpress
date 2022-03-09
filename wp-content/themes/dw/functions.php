d8880d802ff7589dccbcdc0a83c56952bb26d3f3
EmilieColleye <emilie.colleye@student.hepl.be>
2022-03-09 16:46:57 +0100

    Added menu BEM classes generator

--- wp-content/themes/dw/functions.php
+++ wp-content/themes/dw/functions.php
@@ -12,8 +12,7 @@ add_theme_support('post-thumbnails');
 // Enregistrer un seul custom post-type pour "nos voyages"
 register_post_type('trip', [
     'label' => 'Voyages',
-    'labels' => [ 'name' => 'Voyages', 'singular_name' => 'Voyage',
-    ],
+    'labels' => [ 'name' => 'Voyages', 'singular_name' => 'Voyage'],
     'description' => 'Tous les articles qui décrivent un voyage',
     'public' => true,
     'menu_position' => 5,
@@ -41,33 +40,41 @@ function dw_get_trips($count = 20)
 register_nav_menu('primary', 'emplacement de la navigation principale de haut de page');
 register_nav_menu('footer', 'emplacement de la navigation en pied de page');
 
-//définition de la fonction retournant un menu de navigation sous forme d'un tableau de lien de niveau 0
-function dw_get_menu_items($location){
+// Définition de la fonction retournant un menu de navigation sous forme d'un tableau de liens de niveau 0.
+
+function dw_get_menu_items($location)
+{
     $items = [];
-    // récupérer le menu qui correspond à l'emplacement souhaité
+
+    // Récupérer le menu qui correspond à l'emplacement souhaité
     $locations = get_nav_menu_locations();
 
-    // permet de dire est-ce que ce qui précède existe et n'est pas null alors on utilise la 1ere valeur sinon (si la clé n'existe pas) c'est null qui est utilisé
-
-    if($locations[$location] ?? null){
+    if($locations[$location] ?? false) {
         $menu = $locations[$location];
 
-        // récupérer tous les éléments du menu en question
+        // Récupérer tous les éléments du menu en question
         $posts = wp_get_nav_menu_items($menu);
 
-        // traiter chaque élément de menu pour le transformer en objet
-        foreach ($posts as $post){
-            //Créer une instance d'unobjet personnalisé à partir de $post
+        // Traiter chaque élément de menu pour le transformer en objet
+        foreach($posts as $post) {
+            // Créer une instance d'un objet personnalisé à partir de $post
             $item = new PrimaryMenuItem($post);
-            //Ajoute rune instance soit à $items (s'il s'agit d'un élément de niveau 0), soit en tant que sous élément d'un item déjà existant
-            if($item->isSubItem()){
-                //ajouter l'instance
+
+            // Ajouter cette instance soit à $items (s'il s'agit d'un élément de niveau 0), soit en tant que sous-élément d'un item déjà existant.
+            if($item->isSubItem()) {
+                // Ajouter l'instance comme "enfant" d'un item existant
+                foreach($items as $existing) {
+                    if($existing->isParentFor($item)) {
+                        $existing->addSubItem($item);
+                    }
+                }
             } else {
-                $items[]=$item;
+                // Il s'agit d'un élément de niveau 0, on l'ajoute au tableau
+                $items[] = $item;
             }
         }
     }
 
-    // retourner les éléments de menu de niveau 0
+    // Retourner les éléments de menu de niveau 0
     return $items;
 }
--- wp-content/themes/dw/menus/PrimaryMenuItem.php
+++ wp-content/themes/dw/menus/PrimaryMenuItem.php
@@ -1,6 +1,7 @@
 <?php
-class PrimaryMenuItem{
 
+class PrimaryMenuItem
+{
     protected $post;
 
     public $url;
@@ -8,19 +9,68 @@ class PrimaryMenuItem{
     public $title;
     public $subitems = [];
 
-    public function __construct(){
+    public function __construct($post)
+    {
         $this->post = $post;
+
         $this->url = $post->url;
         $this->label = $post->title;
         $this->title = $post->attr_title;
-
     }
 
-    public function hasSubItems(){
-        return !empty($this->subitems);
+    public function hasSubItems()
+    {
+        return ! empty($this->subitems);
     }
 
-    public function isSubItems(){
-        return !empty($this->subitems);
+    public function isSubItem()
+    {
+        return boolval($this->getParentId());
+    }
+
+    public function getParentId()
+    {
+        return $this->post->menu_item_parent;
+    }
+
+    public function isParentFor(PrimaryMenuItem $instance)
+    {
+        return ($this->post->ID == $instance->getParentId());
+    }
+
+    public function addSubItem(PrimaryMenuItem $instance)
+    {
+        $this->subitems[] = $instance;
+    }
+
+    public function getBemClasses($base)
+    {
+        $icon = get_field('icon', $this->post);
+        $modifiers = [];
+
+        if($this->isCurrent()) {
+            $modifiers[] = 'current';
+        }
+
+        if($this->post->type === 'custom') {
+            $modifiers[] = 'url';
+        }
+
+        if($icon) {
+            $modifiers[] = $icon;
+        }
+
+        $value = $base;
+
+        foreach($modifiers as $modifier) {
+            $value .= ' ' . $base . '--' . $modifier;
+        }
+
+        return $value;
+    }
+
+    public function isCurrent()
+    {
+        return ($_SERVER['REQUEST_URI'] == parse_url($this->post->url, PHP_URL_PATH));
     }
 }
